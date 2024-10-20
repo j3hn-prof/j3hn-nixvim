9c32405dcfb317d6110c33a526614c6e375c13cb
j3hn <johnrossi2601@gmail.com>
2024-10-20 11:47:41 -0400

    testing stylix

--- config/default.nix
+++ config/default.nix
@@ -1,6 +1,6 @@
 {
   # Import all your configuration modules here
-  imports = [ ./bufferline.nix ./base16.nix ];
+  imports = [ ./bufferline.nix /*./base16.nix*/ ];
   config.opts = {
 		clipboard = "unnamedplus";
 		shiftwidth = 2;
