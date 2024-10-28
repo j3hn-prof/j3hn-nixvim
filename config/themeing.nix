9ccfb59831fbd4044ea931d21c1f0884ab669c87
j3hn <johnrossi2601@gmail.com>
2024-10-28 19:59:18 -0400

    changed colorscheme to tokyonightmoon

--- config/themeing.nix
+++ config/themeing.nix
@@ -4,25 +4,11 @@ let
 	colorModule = config.colorschemes.nightfox.settings.options.modules;
 in
 {
-	options.colorModule.lightline = lib.mkOption {
-		type = lib.types.boolean;
-	};
-
 	config = {
 		plugins.web-devicons.enable = true;
-		colorschemes.nightfox = {
+		colorschemes.tokyonight = {
 			enable = true;
-			flavor = "carbonfox";
-			luaConfig.post = lib.mkIf colorModule.lightline ''
-				vim.g.lightline = { colorscheme = "carbonfox" }
-			'';
-			settings.options = {
-				modules = {
-					neotree = true;
-					lightline = true;
-				};
-				module_default = false;
-			};
+			settings.style = "moon";
 		};
 		opts.stc = "%s%=%{v:relnum ? v:relnum : v:lnum}│";
 		highlightOverride.CursorLineNr.bold = true;
