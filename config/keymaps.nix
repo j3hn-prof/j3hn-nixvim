de8aa52675daed729a4bfa83abcc07de547ab1b1
j3hn <johnrossi2601@gmail.com>
2024-10-28 23:25:09 -0400

    redid write out keymap

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -22,7 +22,7 @@
           function()
             if vim.bo.modified then
               vim.cmd('write')
-							print("Wrote out to file")
+							print("Wrote out to file ",vim.api.nvim_buf_get_name(0))
             else
               print("Nothing to write")
             end
@@ -31,6 +31,7 @@
 			key = "<C-w>";
 			options = {
 				silent = true;
+				nowait = true;
 			};
 		}	
 	];
