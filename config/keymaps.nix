50f04ced96c2974839a415593e538fcaa1244267
j3hn <johnrossi2601@gmail.com>
2024-10-29 00:29:06 -0400

    reformatted the keymaps.nix and added a helper funciton

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,66 +1,68 @@
 {helpers, ...}:
+let
+	mkLuaFunction = input: helpers.mkRaw
+	''
+		function()
+			${input}
+		end
+	'';
+in
 {
 	keymaps = [
-		{
-			action = ":Neotree toggle<CR>";
+		{ # Neotree Keymap
+			action = mkLuaFunction ''vim.cmd("Neotree toggle")'';
 			key = "<C-n>";
 			options = {
 				silent = true;
 			};
 		}
-		{
-			action = 
-				(helpers.mkRaw 
+		{ # Quit Keymap
+			action = mkLuaFunction 
 				''
-					function()
-						local has_unsaved = false
-						local buffers = vim.api.nvim_list_bufs()
+					local has_unsaved = false
+					local buffers = vim.api.nvim_list_bufs()
 
-						for _, buf in ipairs(buffers) do
-							if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, 'modified') then
-								has_unsaved = true
-								break
-							end
-						end
-
-						if has_unsaved then
-							vim.api.nvim_echo({{"There are unsaved buffers. Press 'y' to quit without saving.", "WarningMsg"}}, true, {})
-							local char = vim.fn.getchar()
-							if char == vim.fn.char2nr('y') then
-								vim.cmd("quitall!")
-							else
-								vim.api.nvim_echo({{"Quit canceled. Buffers are still unsaved.", "Italic"}}, true, {})
-							end
-						else
-							vim.cmd("quit")
+					for _, buf in ipairs(buffers) do
+						if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, 'modified') then
+							has_unsaved = true
+							break
 						end
 					end
-				'');
+
+					if has_unsaved then
+						vim.api.nvim_echo({{"There are unsaved buffers. Press 'y' to quit without saving.", "WarningMsg"}}, true, {})
+						local char = vim.fn.getchar()
+						if char == vim.fn.char2nr('y') then
+							vim.cmd("quitall!")
+						else
+							vim.api.nvim_echo({{"Quit canceled. Buffers are still unsaved.", "Italic"}}, true, {})
+						end
+					else
+						vim.cmd("quitall")
+					end
+				'';
 			key = "<C-q>";
 			options = {
 				silent = true;
 			};
 		}
-		{
-			action = 
-				(helpers.mkRaw
+		{ # Write Keymap
+			action = mkLuaFunction 
         ''
-          function()
-            if vim.bo.modified then
-              vim.cmd('write')
-							vim.api.nvim_echo({
-  							{"Wrote out to file: ", "Normal"},
-								{vim.api.nvim_buf_get_name(0), "Bold"}
-							}, true, {})
-						else
-              vim.api.nvim_echo({
-  							{"Nothing to write.", "Italic"},
-							}, true, {})
+					if vim.bo.modified then
+						vim.cmd('write')
+						vim.api.nvim_echo({
+							{"Wrote out to file: ", "Normal"},
+							{vim.api.nvim_buf_get_name(0), "Bold"}
+						}, true, {})
+					else
+						vim.api.nvim_echo({
+							{"Nothing to write.", "Italic"},
+						}, true, {})
 
-            end
-          end
-        '');
-			key = "<C-w>";
+					end
+        '';
+			key = "<C-s>";
 			options = {
 				silent = true;
 				nowait = true;
