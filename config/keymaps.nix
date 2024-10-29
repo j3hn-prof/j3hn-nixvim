8e535e2e211583286854300e0a0538be93a1fd61
j3hn <johnrossi2601@gmail.com>
2024-10-29 13:07:13 -0400

    reformatted kemaps again

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -23,19 +23,26 @@ in
 					local buffers = vim.api.nvim_list_bufs()
 
 					for _, buf in ipairs(buffers) do
-						if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, 'modified') then
+						if vim.api.nvim_buf_is_valid(buf) 
+						and vim.api.nvim_buf_is_loaded(buf)
+						and vim.api.nvim_buf_get_option(buf, 'modified') 
+						then
 							has_unsaved = true
 							break
 						end
 					end
 
 					if has_unsaved then
-						vim.api.nvim_echo({{"There are unsaved buffers. Press 'y' to quit without saving.", "WarningMsg"}}, true, {})
+						vim.api.nvim_echo({
+							{"There are unsaved buffers. Press 'y' to quit without saving.", "WarningMsg"}
+						}, true, {})
 						local char = vim.fn.getchar()
 						if char == vim.fn.char2nr('y') then
 							vim.cmd("quitall!")
 						else
-							vim.api.nvim_echo({{"Quit canceled. Buffers are still unsaved.", "Italic"}}, true, {})
+							vim.api.nvim_echo({
+								{"Quit canceled. Buffers are still unsaved.", "Italic"}
+							}, true, {})
 						end
 					else
 						vim.cmd("quitall")
