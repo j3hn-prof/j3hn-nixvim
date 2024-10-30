93adcab481bf14c0fbab2f4820f983fbb653262e
j3hn <johnrossi2601@gmail.com>
2024-10-29 20:32:20 -0400

    changed nix lsp to nil_ls

--- config/lsps/default.nix
+++ config/lsps/default.nix
@@ -1,37 +1,8 @@
 {
-  plugins = {
-    lsp-lines.enable = true;
-    lspsaga.enable = true;
-    lsp = {
-      enable = true;
-      servers = {
-        nixd = {
-          enable = true;
-          settings = {
-            nixpkgs = {
-              expr = "import <nixpkgs> {}";
-            };
-            formatting = {
-              command = ["alejandra"];
-            };
-            # options = {
-            #  nixos = {
-            # 		 expr = '(builtins.getFlake "/PATH/TO/FLAKE").nixosConfigurations.CONFIGNAME.options';
-            # };
-            #  home_manager = {
-            # 		 expr = '(builtins.getFlake "/PATH/TO/FLAKE").homeConfigurations.CONFIGNAME.options';
-            #  };
-            # };
-          };
-        };
-      };
-      keymaps.lspBuf = {
-        "gd" = "definition";
-        "gD" = "references";
-        "gt" = "type_definition";
-        "gi" = "implementation";
-        "K" = "hover";
-      };
-    };
-  };
+	plugins = {
+			lsp-lines.enable = true;
+			lspsaga.enable = true;
+			lsp.enable = true;
+			lsp.servers.nil_ls.enable = true;
+	};
 }
